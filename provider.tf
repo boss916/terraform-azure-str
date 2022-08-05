cbb0a6c5534b8e7fed2cdd0b64cee12499c2f88c
Fasil E <muhammad.fasil-e@capgemini.com>
2022-08-05 17:52:49 +0530

    ddd

--- provider.tf
+++ provider.tf
@@ -1,12 +1,9 @@
 provider "azurerm" {
   #skip_provider_registration = "true"
   features {}
-  subscription_id = "b0b416b6-14e9-4ee1-abfe-db712377a334"
-  client_id       = "c5a0c1d2-d6d1-458d-99fa-a85404fadbfd"
-  client_secret   = "5cTP4fC36zCmQaPy81J..uq3VTz~WKW6Zt"
-  tenant_id       = "76a2ae5a-9f00-4f6b-95ed-5d33d77c4d61"
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
 
 }
-
-
-
---
+++ var.tf
@@ -0,0 +1,134 @@
+/*variable "tags" {
+  type = map
+  
+}
+*/
+
+variable "resource_group" {
+  description = "resource group name"
+  type        = string
+  default     = "Shravani_HandsOn_Lab"
+}
+
+variable "location" {
+  description = "location "
+  type        = string
+  default     = "East US"
+}
+/*variable "account_tier" {
+  description = "account tier "
+  type        = string
+  default     = ""
+}*/
+/*variable "endpoint_resource_id" {
+  description = "storage account name"
+  type        = string
+  default     = ""
+}*/
+
+
+
+variable "storage_accounts" {
+  type = map(object({
+    storage_account_name = string
+    #containers_list       =list(object({ name = string, access_type = string }))
+    is_hns_enabled = bool
+    #endpoint_resource_id=string
+  }))
+  default = {
+    one = {
+      storage_account_name = "dlfjdlvkflvkjj22"
+      #containers_list       =list(object({ name = string, access_type = string }))
+      is_hns_enabled = false
+      #endpoint_resource_id="/subscriptions/f9f64129-c103-4fb5-a4b5-1e93865d3427/resourceGroups/Shravani_HandsOn_Lab/providers/Microsoft.KeyVault/vaults/IdeaLZKeyVaultnew045"
+    }
+  }
+}
+
+
+
+
+
+  variable "key_vault_id" {
+    type  = string
+    default =""
+  }
+  
+  /*
+  variable "key_vault_key_name" {
+    type  = string
+    default =""
+  }
+  */
+  /*variable "key_vault_key_name" {
+    type  = string
+    default =""
+  }
+ 
+ 
+  variable "storage_cmk_key_vault_id" {
+    type = string 
+    default =""
+  }
+variable "storage_cmk_key_name" {
+   type = string 
+    default =""
+}
+
+
+
+
+
+variable "key_permissions_storage_one" {
+type =list(string)
+  default = [""]
+  
+  
+}
+
+variable "secret_permissions_storage_one" {
+type = list(string)
+  default = [""]
+}
+
+variable "key_permissions_client_one" {
+  
+default = [""]
+ type = list(string)
+ }
+ variable "secret_permissions_client_one" {
+   default = [""]
+   type = list(string)
+   }
+
+
+
+variable "key_permissions_storage_two" {
+type =list(string)
+  default = [""]
+  
+  
+}
+
+variable "secret_permissions_storage_two" {
+type = list(string)
+  default = [""]
+}
+
+variable "key_permissions_client_two" {
+  
+default = [""]
+ type = list(string)
+ }
+ variable "secret_permissions_client_two" {
+   default = [""]
+   type = list(string)
+  */ #}
+   
+   
+   variable "tags" {
+  description = "The ID of the public subnet"
+  default     = {
+    name="env"
+  }
+}
