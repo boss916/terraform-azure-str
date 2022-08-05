e94519b6246e441e0623da0218ac3041dceeef29
Fasil E <muhammad.fasil-e@capgemini.com>
2022-08-05 14:45:45 +0530

    sdcdf

---
+++ .auto.tfvars
@@ -0,0 +1,7 @@
+tags = {
+    Administrator = "Andy Welsh"
+    Department = "IT"
+    CostCentre = "ABC123"
+    ContactPerson = "andy@tinfoilcipher.co.uk"
+    ManagedByTerraform = "True"
+}
---
+++ provider.tf
@@ -0,0 +1,9 @@
+provider "azurerm" {
+  #skip_provider_registration = "true"
+  features {}
+  subscription_id = ""
+  client_id       = ""
+  client_secret   = ""
+  tenant_id       = ""
+
+}
